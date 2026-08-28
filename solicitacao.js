2c8fb8ba4a179dd2fae8f8939780adc69899084f
Alefesanttos <alefe.santtos2022@gmail.com>
2026-08-28 20:33:03 -0300

    Delete solicitacao.js

--- solicitacao.js
+++
@@ -1,1 +0,0 @@
-function loadRequestV2(id){return apiGet("solicitacao_v2",{token:getSession().token,id})}function createRequestV2(data){return apiPost("criar_solicitacao",{token:getSession().token,...data})}
