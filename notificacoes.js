ca956f9dab473a74345c0da85ff5ce13c5f234af
Alefesanttos <alefe.santtos2022@gmail.com>
2026-08-28 20:34:16 -0300

    Delete notificacoes.js

--- notificacoes.js
+++
@@ -1,1 +0,0 @@
-function loadNotificationsV2(){return apiGet("notificacoes",{token:getSession().token})}function markNotificationV2(id){return apiPost("marcar_notificacao",{token:getSession().token,id})}
