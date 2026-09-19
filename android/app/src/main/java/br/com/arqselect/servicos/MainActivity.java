7764a0fe0fdd1c154786500ef1365c7998924e37
Alefesanttos <alefe.santtos2022@gmail.com>
2026-09-19 11:46:58 -0300

    Add ARQSELECT Servicos Android app

---
+++ android/app/src/main/java/br/com/arqselect/servicos/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.arqselect.servicos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+}
